bbe9912e71063fd460aa5607939761fbe3c1a4dd
Rafay <rafayalishah74@gmail.com>
2026-06-17 12:45:54 +0500

    Force secure cookies for cross-origin authentication

--- server/src/utils/generateToken.ts
+++ server/src/utils/generateToken.ts
@@ -15,8 +15,8 @@ export const generateToken = (res: Response, userId: mongoose.Types.ObjectId | s
 
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    secure: true,
+    sameSite: 'none',
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 };
