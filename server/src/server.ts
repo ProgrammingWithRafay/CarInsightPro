0415af08f8f14600976c60b1c67dc05cbba5a031
Rafay <rafayalishah74@gmail.com>
2026-06-18 18:20:03 +0500

    Nodemailer Fixes

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,12 @@ import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 import rateLimit from 'express-rate-limit';
 import dotenv from 'dotenv';
+import dns from 'dns';
+
+// Force Node.js to use IPv4 first to prevent ENETUNREACH errors in Docker/Railway
+// environments that do not have outbound IPv6 routing enabled.
+dns.setDefaultResultOrder('ipv4first');
+
 import { connectDB } from './config/db';
 import authRoutes from './routes/authRoutes';
 import carRoutes from './routes/carRoutes';
